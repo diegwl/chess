67f64743e4922bb737d8d25a7386736b2e0fcf72
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2023-09-18 08:16:29 -0300

    Clear Screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -13,14 +15,20 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println("\nSource: ");
-            ChessPosition source = UI.readChessPostion(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println("\nSource: ");
+                ChessPosition source = UI.readChessPostion(sc);
 
-            System.out.println("\nTarget: ");
-            ChessPosition target = UI.readChessPostion(sc);
+                System.out.println("\nTarget: ");
+                ChessPosition target = UI.readChessPostion(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException | InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
 }
