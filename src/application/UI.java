a65e2706a57e1e61ab8b2ca746d06734e52a358c
Lopes Diego (CaP/ETS) <lod7ca@bosch.com>
2023-09-18 09:45:38 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPosition;
 import chess.Color;
 import chess.ChessPiece;
@@ -44,6 +45,13 @@ public class UI {
         } catch (RuntimeException e) {
             throw new InputMismatchException("Error reading ChessPosition");
         }
+    }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
